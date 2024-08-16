eb40c45e5d5a5fd98303a290957c706c5b9e1faf
Sara <sarafarris03@gmail.com>
2024-08-16 17:12:09 +0200

    add dashboard

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,15 @@ from dash import Dash, dcc, ctx, html, Input, Output, State
 import dash_bootstrap_components as bootstrap
 from dash.long_callback import DiskcacheLongCallbackManager
 import diskcache
+from dash import dash_table
+import dash
+import pandas as pd
 from src.retrieve_file import retrieve_options
+from src.add_file import insert_file_dashboard
+from src.retrieve_file import filter_subject_dashboard
+from src.retrieve_file import filter_school_dashboard
+from src.retrieve_file import filter_subject_school_dashboard
+
 
 
 cache = diskcache.Cache("./cache")
@@ -12,62 +20,64 @@ app = Dash(__name__, long_callback_manager=long_callback_manager, external_style
 
 
 list_path = "Database/list_options.csv"
+data_path = "Database/database.csv"
 
+df = pd.read_csv(data_path)
+df = df[['FileName', 'Path', 'Subject', 'School', 'Year']]
 
 def get_list_subject_and_school(list_path):
 
     subjects = retrieve_options(list_path)[0]
     schools = retrieve_options(list_path)[1]
 
-    children_subject = []
-    children_school = []
-
-    for subject in subjects:
-        children_subject.append(bootstrap.DropdownMenuItem(subject))
-
-    for school in schools:
-        children_school.append(bootstrap.DropdownMenuItem(school))
-
-    return children_subject, children_school
+    return subjects,schools
 
 
 children_subject, children_school = get_list_subject_and_school(list_path)
 
-
+#[x for x in sorted(df.Subject.unique())],
 #Structure of the page
 
 header = bootstrap.Navbar(bootstrap.Container(html.H3("Jessica's School Library", style={'color': 'white'}, className="text-center"), fluid=True), dark=True, color='#c71585', sticky='top')
 
-table_header = [
-    html.Thead(html.Tr([html.Th("File Name"), html.Th("Path File"), html.Th("Subject"), html.Th("School"), html.Th("Year")]))
-]
-
-row1 = html.Tr([html.Td("Arthur"), html.Td("Dent")])
-row2 = html.Tr([html.Td("Ford"), html.Td("Prefect")])
-row3 = html.Tr([html.Td("Zaphod"), html.Td("Beeblebrox")])
-row4 = html.Tr([html.Td("Trillian"), html.Td("Astra")])
-
-table_body = [html.Tbody([row1, row2, row3, row4])]
 
 content = bootstrap.Container([
     bootstrap.Row(bootstrap.Col(html.Div("Look for a file here"),  style={"padding-top": "4%", "font-size": "24px"})),
     bootstrap.Row([
 
         bootstrap.Col([
-            bootstrap.DropdownMenu(label="Subject", children=children_subject, color='danger', className='mb-3', style={"padding-top": "4%"})
+            subject_drop := dcc.Dropdown(options = [x for x in sorted(df.Subject.unique())], placeholder="Subject", className='mb-3', style={"padding-top": "4%"})
         ]),
 
         bootstrap.Col([
-            bootstrap.DropdownMenu(label="School", children=children_school, className='mb-3', style={"padding-top": "4%"})
+            school_drop := dcc.Dropdown(options = [x for x in sorted(df.School.unique())], placeholder="School", className='mb-3', style={"padding-top": "4%"})
         ])
     ]),
 
     bootstrap.Row([
+        #(table_header + table_body, bordered=True, style={"padding-top": "4%"})
+        my_table :=
+            dash_table.DataTable(
+                columns=[
+                    {'name': "FileName", 'id': 'FileName', 'type': 'text'},
+                    {'name': "Path", 'id': 'Path', 'type': 'text'},
+                    {'name': "Subject", 'id': 'Subject', 'type': 'text'},
+                    {'name': "School", 'id': 'School', 'type': 'text'},
+                    {'name': "Year", 'id': 'Year', 'type': 'numeric'},
+                ],
+                data=df.to_dict('records'),
+                filter_action='native',
+                page_size=10,
 
-        bootstrap.Col([
-            bootstrap.Table(table_header + table_body, bordered=True, style={"padding-top": "4%"})
+                style_data={
+                    'width': '150px', 'minWidth': '150px', 'maxWidth': '150px',
+                    'overflow': 'hidden',
+                    'textOverflow': 'ellipsis',
+                    'scroll-y': 'overflow'
+                }
+            )
 
-        ])
+
 
     ]),
 
@@ -75,29 +85,31 @@ content = bootstrap.Container([
     bootstrap.Row([
         bootstrap.Col([
             bootstrap.InputGroup(
-                [bootstrap.InputGroupText("File Name"), bootstrap.Input(placeholder="Write the name of the file"), ],
+                [bootstrap.InputGroupText("File Name"), bootstrap.Input(placeholder="Write the name of the file", id='InsertFileName' ), ],
                 className="mb-3", size="lg", style={"padding-top": "2%"}
             ),
 
             bootstrap.InputGroup(
-                [bootstrap.InputGroupText("File path"), bootstrap.Input(placeholder="Write the path of the file")],
-                className="mb-3", size="lg",
+                [bootstrap.InputGroupText("File path"), bootstrap.Input(placeholder="Write the path of the file" , id='InsertFilePath')],
+                className="mb-3", size="lg"
             ),
 
             bootstrap.InputGroup(
-                [bootstrap.InputGroupText("Subject"), bootstrap.Input(placeholder="Write the subject name")],
-                className="mb-3", size="lg",
+                [bootstrap.InputGroupText("Subject"), bootstrap.Input(placeholder="Write the subject name", id='InsertSubject')],
+                className="mb-3", size="lg"
             ),
 
             bootstrap.InputGroup(
-                [bootstrap.InputGroupText("School"), bootstrap.Input(placeholder="Write the school name")],
-                className="mb-3", size="lg",
+                [bootstrap.InputGroupText("School"), bootstrap.Input(placeholder="Write the school name", id='InsertSchool')],
+                className="mb-3", size="lg"
             ),
 
             bootstrap.InputGroup(
-                [bootstrap.InputGroupText("Year"), bootstrap.Input(placeholder="Write the year")],
-                className="mb-3", size="lg",
+                [bootstrap.InputGroupText("Year"), bootstrap.Input(placeholder="Write the year", id='InsertYear')],
+                className="mb-3", size="lg"
             ),
+
+            bootstrap.Button("Add file", color="success", className="me-1", size="lg", id="addbutton")
         ]),
 
         bootstrap.Col(
@@ -105,7 +117,7 @@ content = bootstrap.Container([
                 [
                     bootstrap.CardImg(src= "https://varthana.com/school/wp-content/uploads/2023/04/B296.jpg", top=True),
                     bootstrap.CardBody(
-                        html.P("Teaching for a better future", className="card-text")
+                        html.P("Teaching for a better future", className="card-text", style={"font-size": "20px"} )
                     ),
                 ],
                 style={"width": "18rem"},
@@ -115,6 +127,70 @@ content = bootstrap.Container([
 
 ])
 app.layout = html.Div([header, content])
+
+@app.callback(
+    Output(component_id=subject_drop,component_property="options"),
+    Output(component_id=school_drop, component_property="options"),
+    State(component_id="InsertFileName", component_property="value"),
+    State(component_id="InsertFilePath", component_property="value"),
+    State(component_id="InsertSubject", component_property="value"),
+    State(component_id="InsertSchool", component_property="value"),
+    State(component_id="InsertYear", component_property="value"),
+    Input(component_id="addbutton", component_property="n_clicks")
+)
+def insert_callback(file_name = None, file_path = None, subject = None, school = None, year = None, click = None):
+    print(click)
+    if click is not None:
+        if click > 0:
+            #updated the two file csv with the new elements
+            insert_file_dashboard(data_path, list_path, file_name, file_path, subject, school, year)
+            dff = pd.read_csv(data_path)
+            dff = dff[['FileName', 'Path', 'Subject', 'School', 'Year']]
+            subjects = [x for x in sorted(dff.Subject.unique())]
+            schools = [x for x in sorted(dff.School.unique())]
+            return subjects, schools
+    else:
+        return [x for x in sorted(df.Subject.unique())],[x for x in sorted(df.School.unique())]
+
+
+
+@app.callback(
+    Output(my_table, 'data'),
+    #Output(my_table, 'page_size'),
+    Input(subject_drop, 'value'),
+    Input(school_drop, 'value'),
+    Input('addbutton', 'n_clicks')
+)
+def retrieve_file(subject, school, click):
+
+    if click:
+        dff = pd.read_csv(data_path)
+        dff = dff[['FileName', 'Path', 'Subject', 'School', 'Year']]
+        return dff.to_dict('records')
+
+    else:
+        dff = df.copy()
+
+    if school and subject:
+        dff = dff[dff['Subject'] == subject]
+        dff = dff[dff['School'] == school]
+        print("DataFrame after filtering:\n", dff)
+        return dff.to_dict('records')
+
+    elif subject:
+        dff = dff[dff['Subject'] == subject]
+        print("DataFrame after filtering:\n", dff)
+        return dff.to_dict('records')
+
+    elif school:
+        dff = dff[dff['School'] == school]
+        print("DataFrame after filtering:\n", dff)
+        return dff.to_dict('records')
+
+
+
+    return dff.to_dict('records')
+
 
 
 if __name__ == '__main__':
